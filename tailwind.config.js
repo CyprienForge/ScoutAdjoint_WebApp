784810b70076602f2e5240237441fa11646c736e
Cyprien <cyprien.forge@gmail.com>
2025-06-15 17:34:27 +0200

    :heavy_plus_sign: - Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        './src/Infrastructure/Twig/templates/**/*.twig',
+        './assets/controllers/**/*.js',
+        './assets/**/*.js',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
